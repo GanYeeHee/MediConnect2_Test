4b028464b1f539ea48839bd2fc6e07155dfaf563
yeehee <ganyeehee@gmail.com>
2025-09-06 17:58:21 +0800

    Update EditProfile.kt

--- app/src/main/java/com/example/testasgn/ui/userTheme/EditProfile.kt
+++ app/src/main/java/com/example/testasgn/ui/userTheme/EditProfile.kt
@@ -39,8 +39,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun EditProfile(navController: NavController) {
